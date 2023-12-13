e18d8ef5d3ea5328046258a30e0479b358babecc
atai20 <37374053+atai20@users.noreply.github.com>
2023-12-13 07:57:31 -0500

    third

--- mysite/userposts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserpostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userposts'
--- mysite/userposts/choices.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Vote(models.IntegerChoices):
-    UPVOTE = 1
-    DOWNVOTE = -1
-
-class Tags(models.TextField):
-    MY_TEXT = 'sdf'
