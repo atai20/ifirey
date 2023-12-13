92d98d896c1802fed2d8388892e0b2c98ad1c640
atai20 <37374053+atai20@users.noreply.github.com>
2023-12-13 07:38:13 -0500

    second

---
+++ mysite/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Profile(models.Model):
+    user = models.CharField(max_length=200)
+
+    avatar = models.ImageField(default='default.jpg', upload_to='img')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user
